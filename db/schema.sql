adb4f92e771c58aae940b7c5735a1d052c7e84c4
Mason Walker <masonwalker30@gmail.com>
2022-01-31 00:11:12 -0800

    seeds added

--- db/schema.sql
+++ db/schema.sql
@@ -14,8 +14,8 @@ CREATE TABLE role(
     title VARCHAR(30),
     salary DECIMAL,
     dept_id INT,
-    FOREIGN KEY (dept_id),
-    REFERENCES department(id),
+    FOREIGN KEY (dept_id)
+    REFERENCES department(id)
     ON DELETE SET NULL
 );
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,26 @@
 INSERT INTO
     department (id, name)
-VALUES
+VALUES(1, "Awaiting Underwriting"),
+    (2, "Examination"),
+    (3, "Attorney Hold"),
+    (4, "Delivered"),
 
 
 INSERT INTO
-
-VALUES
-
+    role (id, title, salary, department_id)
+VALUES(1, "Examiner", 80000, 2),
+    (2, "Attorney", 120000, 3),
+    (3, "Underwriter", 15000, 4),
+    (4, "Commercial Examiner", 120000, 1),
 
 INSERT INTO 
-
-VALUES
+    employee (id, first_name, last_name, role_id, manager_id)
+VALUES(1, "Sabrina", "Salinas", 1,7),
+    (2, "Evan", "Boles", 1,7),
+    (3, "Cody", "Kimbal", 1,7),
+    (4, "David", "Benett", 4,5),
+    (5, "Lex", "Grossman", 2,NULL),
+    (6, "Laurie", "Tipton", 4,5),
+    (7, "Ellen", "Gunter", 3,NULL),
+    (8, "Jordan", "Belfort", 2,NULL),
+    (9, "Heidi", "Riano", 3,8),
