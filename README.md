822dd452186a6e7d5c5a785ccbb4e71afecd4bfa
Mason Walker <masonwalker30@gmail.com>
2022-04-17 21:22:37 -0700

    readme edit

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Team-Member-Tracker
+
+Live Link: 
+Repo Link: https://github.com/charliem61/Team-Member-Tracker
+
+Goal/Motivation:
+
+To create an application using SQL that allows non developers to interact with preloaded data about their employees. Each will have a manager, a salary, and a role. If this app were created for a buisness owner, they could view and manage each department, roles, and employees so that they could further organize and plan their business. 
+
+Problems Solved:
+
+The application solves all criteria asked in this homework. When the application is started the employer or business owner is presented with options to view all departments, view all roles, view all employees, add a department, add a role, add an employee, or update a an employee. 
+
+After entering data the business is presented with a formatted table showing the department ids and department names, job titles, and role ids.
+
+The Standouts:
+
+The clean working functionality, the video displays the data entered in the table format once complete.
+
+Challenges Faced:
+
+Learning Sql. Creating the tables and formatting the ids and manager codes to match with the correct corresponding employee. 
+
+Formatting and creating the schema file correctly to ensure capatilization was set up to flow correctly to the corresponding key.
+
+Demonstration:
--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function StartMenu() {
         case "Add Role":
           AddRole();
           break;
-         case "Add Employee":
+        case "Add Employee":
           AddEmployee();
           break;
       }
@@ -51,43 +51,92 @@ async function ViewDepartment() {
 StartMenu();
 
 async function AddDepartment() {
-  const departmentResponse = await inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "Enter new department name?",
-      },
-    ])
-    const newDeptAdd=await db.query("INSERT INTO department SET ?",departmentResponse)
-    StartMenu();
-
+  const departmentResponse = await inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "Enter new department name?",
+    },
+  ]);
+  const newDeptAdd = await db.query(
+    "INSERT INTO department SET ?",
+    departmentResponse
+  );
+  StartMenu();
 }
 async function AddRole() {
-  const departments=await db.query("SELECT id as value, name as name FROM DEPARTMENT")
-  const roleResponse = await inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "title",
-        message: "Enter new role name?",
-      }, 
-      {
-        type: "input",
-        name: "salary",
-        message: "Enter new salary?",
-      },
-      {
-        type: "list",
-        name: "department_id",
-        message: "Choose department?",
-        choices: departments
-      },
-    ])
-    const newRoleAdd=await db.query("INSERT INTO role SET ?",roleResponse)
-    StartMenu();
-
+  const departments = await db.query(
+    "SELECT id as value, name as name FROM DEPARTMENT"
+  );
+  const roleResponse = await inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "Enter new role name?",
+    },
+    {
+      type: "input",
+      name: "salary",
+      message: "Enter new salary?",
+    },
+    {
+      type: "list",
+      name: "department_id",
+      message: "Choose department?",
+      choices: departments,
+    },
+  ]);
+  const newRoleAdd = await db.query("INSERT INTO role SET ?", roleResponse);
+  StartMenu();
+}
+async function ViewRoles() {
+  const role = await db.query("SELECT * from role");
+  console.table(role);
+  StartMenu();
 }
 
-/// BUILD VIEW ROLES FUNCTION
-/// BUILD VIEW EMPLOYESS FUNCTION
+async function AddEmployee() {
+  const Roles = await db.query("SELECT * from role");
+  const roleData = Roles.map((role) => {
+    return { name: role.title, value: role.id };
+  });
+  const employeeAdd = await inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "title",
+        message: "Choose a role",
+        choices: roleData,
+      },
+      {
+        type: "input",
+        name: "first_name",
+        message: "Enter their first name",
+      },
+      {
+        type: "input",
+        name: "last_name",
+        message: "Enter their last name",
+      },
+      {
+        type: "input",
+        name: "manager_id",
+        message: "Enter the id of their manager",
+      },
+    ])
+    .then(async function (data) {
+      let { title, first_name, last_name, manager_id } = data;
+      const dataSet = await db.query("INSERT INTO employee SET ?", {
+        role_id: data.title,
+        first_name: data.first_name,
+        last_name: data.last_name,
+        manager_id: data.manager_id,
+      });
+    });
+  StartMenu();
+}
+async function ViewEmployees() {
+  const employee = await db.query("SELECT * FROM employee");
+  console.table(employee);
+  StartMenu();
+}
